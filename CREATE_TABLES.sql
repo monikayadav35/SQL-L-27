5a03f7f30b91056a7fc0e1942ad12c4d063553ab
monika yadav <monikayadaqv@gmail.com>
2026-05-25 15:13:10 +0530

    Create tables and insert data for various data types
    Added SQL commands to create and manipulate various tables demonstrating different data types including numeric, float, double, char, and varchar.

--- CREATE_TABLES.sql
+++ CREATE_TABLES.sql
@@ -105,6 +105,66 @@ insert into student values(1,'naina','10'),(2,'abhi',12);
 select * from student;
 
 
+-- -----------------------------
+create database if not exists datatypedb;
+
+use datatypedb;
+
+-- numberic datatype:----
+
+create table test1(id tinyint);
+insert into test1 values (-1) ,(10);
+insert into test1 values (-128);
+insert into test1 values (-129); -- out of range(1 byte ==> 8 bit ==> 2**8(-128 to 127))
+select * from test1;
+
+-- small int --> 2 byte medium int --> 3 byte
+-- int -->4
+-- big int -->8 byte 
+
+create table test2(age tinyint unsigned);
+insert into test2 values (250);
+select * from test2;
+
+drop table test3;
+create table test3(salary float,salary2 double);
+insert into test3 values(1989.9301934,1989.9301934);
+select * from test3;
+
+
+create table test4(price double(6,2)); -- total is 6,2 is decimal value,4 are the value before
+insert into test4 values(0.6765695);-- only 2 decimal values 
+insert into test4 values(44.7326723);
+insert into test4 values(4453.7326723);
+insert into test4 values(4453);
+insert into test4 values(44536);-- 5 integers
+select * from test4;
+
+
+-- string data type:---
+create table test5(countrycode char(3));
+insert into test5 values('IND');
+insert into test5 values ('INDIA');--   error  <-------------
+insert into test5 values('he      ');
+select * ,char_length(countrycode) from test5;-- (char_length white spaces ko renove kr deta hai at last me )
+select * from test5;
+
+
+-- varchar---------------
+drop  table test6;
+create table test6(countrycode varchar(3));
+insert into test6 values('IND');
+insert into test6 values ('INDIA');--   error  <-------------
+insert into test6 values('h     ');
+
+select * , char_length(countrycode) from test6;
+
+select * from test6;
+
+
+
+
+
 
 
 
